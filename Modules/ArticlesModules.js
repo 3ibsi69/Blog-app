ceefb69c6abcb27d174ba851fe1add14eca9dafe
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-19 11:14:34 +0100

    changed name

--- Modules/ArticlesModules.js
+++ Modules/ArticlesModules.js
@@ -3,7 +3,7 @@ const mongoose = require('./Connection');
 const Blog=mongoose.model('Blog',{
     title:String,
     description:String,
-    pic:String,
+    picture:String,
     date:{
         type:Date,
         default:Date.now()
