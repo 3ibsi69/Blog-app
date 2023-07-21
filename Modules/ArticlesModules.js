748802bf3d19ac76605ef3456326210c6ee385a8
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-21 18:29:01 +0100

    fixed

--- Modules/ArticlesModules.js
+++ Modules/ArticlesModules.js
@@ -3,11 +3,7 @@ const mongoose = require('./Connection');
 const Blog=mongoose.model('Blog',{
     title:String,
     description:String,
-    picture:String,
-    date:{
-        type:Date,
-        default:Date.now()
-    }
+    img:String,
 })
 
 
