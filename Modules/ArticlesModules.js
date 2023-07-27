15c598a3704f202e2265f20c3cb1498fed7d7fe4
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-27 12:25:34 +0100

    .

--- Modules/ArticlesModules.js
+++ Modules/ArticlesModules.js
@@ -1,9 +1,9 @@
 const mongoose = require('./Connection');
 
 const BlogSchema = new mongoose.Schema({
-  title: [String], // Array of titles
-  description: [String], // Array of descriptions
-  img: [String], // Array of images
+  title: [String],
+  description: [String],  
+  img: [String], 
 });
 
 const Blog = mongoose.model('Blog', BlogSchema);
