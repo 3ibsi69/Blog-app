6a54444398930bee81787f5591c267d072c2fcb2
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-27 12:25:05 +0100

    fix accept more data

--- Modules/ArticlesModules.js
+++ Modules/ArticlesModules.js
@@ -1,12 +1,11 @@
 const mongoose = require('./Connection');
 
-const Blog=mongoose.model('Blog',{
-    title:String,
-    description:String,
-    img:String,
-})
+const BlogSchema = new mongoose.Schema({
+  title: [String], // Array of titles
+  description: [String], // Array of descriptions
+  img: [String], // Array of images
+});
 
+const Blog = mongoose.model('Blog', BlogSchema);
 
-
-
-module.exports=Blog;
+module.exports = Blog;
